16d52481a6998e01e2667aff695ace07bec99147
VARSHIT MANEPALLI <varshitmanepalli1810@gmail.com>
2024-05-03 15:53:41 -0400

    All Files Upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model
+model = pickle.load(open('iris_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get values from form
+    input_features = [float(x) for x in request.form.values()]
+    features = [np.array(input_features)]
+    prediction = model.predict(features)
+    output = prediction[0]
+
+    return render_template('index.html', prediction_text='The predicted Iris species is {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
